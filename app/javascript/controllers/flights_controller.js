93b41c7200976dfbb5fa71b37b16b488d25e6f16
Jared Ning <jared@redningja.com>
2024-10-23 15:40:41 -0500

    Move tooltip to duration bar.

--- app/javascript/controllers/flights_controller.js
+++ app/javascript/controllers/flights_controller.js
@@ -107,7 +107,10 @@ export default class extends Controller {
 
   initTooltips() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const tooltipOptions = {
+      html: true,
+    }
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, tooltipOptions))
   }
 
 }
