1698fc002b7b939f0318bf422590378f5271f9cd
Jared Ning <jared@redningja.com>
2024-10-24 19:42:37 -0500

    Add Fare#point_value, kpoints.

--- app/models/fare.rb
+++ app/models/fare.rb
@@ -1,3 +1,12 @@
 class Fare < ApplicationRecord
   belongs_to :flight
+
+  def kpoints
+    (points / 1000.to_f).round
+  end
+
+  def point_value
+    return unless points && cash
+    (points / cash.to_f * 100).round
+  end
 end
